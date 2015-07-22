88775624730634c7c5f3a84c381011a9fc5f790c
Trevor Dorsey <trevor@cloversites.com>
2015-07-21 18:06:21 -0700

    Bumping version

--- cirrus66.js
+++ cirrus66.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cirrus66
 // @namespace    https://github.com/tsdorsey/cirrus66
-// @version      0.1.1
+// @version      0.2.0
 // @description  Organize the dashboard to be a bit more compact.
 // @author       Trevor Dorsey
 // @match        https://app.cloud66.com/dashboard
